bb3f90c2b903bdcb96a815cf48084688c0475bb5
Matthew Wilson <mathwilson45@hotmail.com>
2016-04-16 21:50:33 -0400

    Fixed collision for spheres table naming Added collision detection between walls and spheres

--- Main.py
+++ Main.py
@@ -4,11 +4,11 @@ from visual import *
 
 def collision_detection_sphere(sphere_table):
     #Detects collisions bettwen diffrent spheres
-    for i in range(0,len(object_table)):
-        for n in range(0,len(object_table)):
+    for i in range(0,len(sphere_table)):
+        for n in range(0,len(sphere_table)):
             if n!=i:
-                if abs(object_table[i].pos-object_table[n].pos)<(object_table[i].radius*2):
-                    linear_momentum(object_table[i],object_table[n])
+                if abs(sphere_table[i].pos-sphere_table[n].pos)<(sphere_table[i].radius*2):
+                    linear_momentum(sphere_table[i],sphere_table[n])
 
 def linear_momentum(object1,object2):
     #Assumes elastic collision
@@ -22,3 +22,11 @@ def linear_momentum(object1,object2):
     object1.velocity.x=b
     object2.velocity.y=c
     object1.velocity.y=d
+
+def collision_detection_wall(wall_table,sphere_table):
+    for w in range(0,len(wall_table)):
+        for s in range(0,len(sphere_table)):
+            if abs(wall_table[w].pos.x-sphere_table[s].pos.x)<(sphere_table[s].radius+wall_table[w].size.x)
+                sphere_table[s].velocity.x=-sphere_table[s].velocity.x
+            if abs(wall_table[w].pos.y-sphere_table[s].pos.y)<(sphere_table[s].radius+wall_table[w].size.y)
+                sphere_table[s].velocity.y=-sphere_table[s].velocity.y
